6b343f0431a66b48eebd5fd52ed06efe27f97e04
Anubhav Pathak <anubhavpathak99@gmail.com>
2023-02-04 23:10:17 +0530

    Added Alumni Model

--- models/alumni.mjs
+++ models/alumni.mjs
@@ -1,7 +1,15 @@
-import mongoose from "mongoose";
+import mongoose, { Schema } from "mongoose";
 
 const alumniSchema = mongoose.Schema({
-    
+    memberId: {
+        type: Schema.Types.ObjectId, 
+        required: true, 
+        ref: "member"
+    },
+    quote: Schema.Types.String,
+    description: Schema.Types.Array
 });
+
+const Alumni = mongoose.model("alumni", alumniSchema)
 
 export default alumniSchema;
