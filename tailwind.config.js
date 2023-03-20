32bc5263953620cbbd9ed730d0ded945503b0bb6
Anubhav Pathak <anubhavpathak99@gmail.com>
2023-03-20 20:21:40 +0530

    :fire: Removed key files

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,40 +4,10 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'dark-blue' : '#09141F',
-        'robo-red' : "#FF0000"
       },
-      spacing: {
-        'img': '42rem',
-        'bot1': '31rem',
-        'bot2': '20rem',
-        'himg': '38rem'
-      },
-      keyframes: {
-        jump1: {
-          '0%' : {top: '0rem', left: '25rem'},
-          '75%' : {top: '-11rem', left: '31.5rem'},
-          '100%' : {top: '-10rem', left: '31rem'}
-        },
-        jump2: {
-          '0%' : {top: '0rem'},
-          '75%' : {top: '-14rem'},
-          '100%' : {top: '-12rem'}
-        },
-        go: {
-          '0%' : {scale: 1},
-          '100%': {scale: 0, display: 'none'},
-        },
-        come: {
-          '0%' : {scale: 0, display: 'block'},
-          '100%': {scale: 1},
-        }
-      },
-      animation: {
-        'jumping-bot1': 'jump1 .7s ease-in',
-        'jumping-bot2': 'jump2 .7s ease-in',
-        'icon-go': 'go .2s ease-in forwards',
-        'icon-come': 'come .2s ease-in forwards'
+      fontFamily: {
+        'primary': ['eurofighter', 'Times'],
+        'secondary' : ['exo', 'sans-serif']
       }
     },
   },
