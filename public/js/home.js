dbb883e1bbbd4a5bb39439339fa50914a6cec390
vaibhavraj-exe <vraj.exe@gmail.com>
2023-03-25 11:27:09 +0530

    :sparkles: count up animation

--- public/js/home.js
+++ public/js/home.js
@@ -1,47 +1,49 @@
 import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.esm.browser.min.js'
 
 const swiper = new Swiper('.swiper-sponsor', {
-    loop: true,
-    autoplay: {
-        delay: 1000,
-        disableOnInteraction: false,
+  loop: true,
+  autoplay: {
+    delay: 1000,
+    disableOnInteraction: false,
+  },
+  slidesPerView: 1,
+  spaceBetween: 10,
+  breakpoints: {
+    280: {
+      slidesPerView: 1,
+      spaceBetween: 20
     },
-    slidesPerView: 1,
-    spaceBetween: 10,
-    breakpoints: {
-      280: {
-        slidesPerView: 1,
-        spaceBetween: 20
-      },
-      480: {
-        slidesPerView: 2,
-        spaceBetween: 30
-      },
-      768: {
-        slidesPerView: 2,
-        spaceBetween: 40
-      },
-      992: {
-        slidesPerView: 4,
-        spaceBetween: 40
-      }
+    480: {
+      slidesPerView: 2,
+      spaceBetween: 30
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 40
+    },
+    992: {
+      slidesPerView: 4,
+      spaceBetween: 40
     }
+  }
 });
 
 const swiperSponsor = new Swiper('.swiper-project', {
   loop: true,
   autoplay: {
-      delay: 5000,
+    delay: 5000,
   },
   slidesPerView: 1,
 });
+
+// Lazy loading
 
 const targets = document.querySelectorAll('#images img');
 
 const options = {
   root: document.querySelector('[data-scroll-root]'),
   rootMargin: "500px",
-  threshold: 0
+  threshold: 1
 }
 
 const lazyLoad = target => {
@@ -56,7 +58,7 @@ const lazyLoad = target => {
         img.setAttribute('src', src);
         img.classList.add('load');
 
-        // observer.disconnect();
+        observer.disconnect();
       }
     });
   }, options);
@@ -64,4 +66,54 @@ const lazyLoad = target => {
   io.observe(target)
 };
 
-targets.forEach(lazyLoad); 
+targets.forEach(lazyLoad);
+
+
+
+
+// Stat Count
+
+window.addEventListener('load', function(){
+  var members_count = document.getElementById("members_count");
+  var participations_count = document.getElementById("participations_count");
+  var robots_count = document.getElementById("robots_count");
+  var alumini_count = document.getElementById("alumini_count");
+  
+  function count_intersection_animation(element, x, t) {
+
+    function increase(element = {}, x) {
+      let counts = setInterval(updated, t);
+      let upto = 0;
+    
+      function updated() {
+          element.innerHTML = ++upto + "+";
+          if (upto === x) {
+              clearInterval(counts);
+          }
+      }
+    }
+
+
+    const callback = (entries, observer) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          increase(element, x);
+        }
+      })
+    }
+  
+    const options = {
+      root: null,
+      rootMargin: '0px',
+      threshold: 1
+    }
+    const myObserver = new IntersectionObserver(callback, options)
+    myObserver.observe(element)
+  }
+  
+  count_intersection_animation(members_count, 65, 1000/65)
+  count_intersection_animation(participations_count, 6, 1000/6)
+  count_intersection_animation(robots_count, 11, 1000/11)
+  count_intersection_animation(alumini_count, 100, 1000/100)
+  
+})
